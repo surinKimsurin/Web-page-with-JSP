e4b2ccadb88b9d396f36eaf28a63eee1d1809f16
surinKimsurin <ronica8558@naver.com>
2019-03-13 14:10:27 +0900

    board

---
+++ eclipse-workspace/board/src/main/java/com/myspring/domain/BoardVO.java
@@ -0,0 +1,15 @@
+package com.myspring.domain;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updateDate;
+}
